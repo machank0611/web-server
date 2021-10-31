77e8fd6c55d7265829d698c64cfa0a8a55bbc823
machank0611 <masaki.akiyama@protonmail.com>
2021-10-31 09:39:54 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require("path")
+const app = express();
+app.use(express.urlencoded({ extended: false}))
+app.use(express.static(path.join(__dirname,"public")))
+// app.get('/', function (req, res) {
+//   res.send('<h1>トップページ!!</h1>');
+// });
+
+app.post('/api/v1/quiz', function (req, res) {
+  const answer = req.body.answer;
+  if(answer === "ロバート秋山"){
+    res.send("<h1>正解</h1>");
+  }else{
+    res.send("<h1>不正解</h1>");
+  }
+  });
+ 
+
+app.get('/api/v1/users', function (req, res) {
+  res.send({
+    name:"Mike",
+    age:30
+  });
+});
+ 
+app.listen(3000,function(){
+  console.log("I am running!")
+});
